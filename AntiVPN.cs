acc745603665519acef4b83c13758c9400abd58c
Justin Noble <justin58505@gmail.com>
2025-11-03 22:16:25 -0600

    Update API version, various formatting changes, sanity checks

--- AntiVPN.cs
+++ AntiVPN.cs
@@ -22,8 +22,8 @@ public class AntiVPN : BasePlugin
         {
             foreach (var player in Utilities.GetPlayers())
             {
-                if (player == null || !player.IsValid || player.IsBot || player.IsHLTV || player.AuthorizedSteamID == null) continue;
-                CheckPlayerIP(player);
+                if (!player.IsValid || player.IsBot || player.IsHLTV || player.AuthorizedSteamID == null) continue;
+                CheckPlayerIp(player);
             }
         }
     }
@@ -37,56 +37,49 @@ public class AntiVPN : BasePlugin
 
         AddTimer(1f, () =>
         {
-            CheckPlayerIP(player);
+            CheckPlayerIp(player);
         });
 
         return HookResult.Continue;
     }
     
-    public void CheckPlayerIP(CCSPlayerController player)
+    private void CheckPlayerIp(CCSPlayerController player)
     {
-        string ipAddress = player!.IpAddress!.Split(":")[0];
-        string steamid = player!.AuthorizedSteamID!.SteamId64.ToString();
+        if (!player.IsValid) return;
+        var ipAddress = player.IpAddress?.Split(":")[0] ?? "error";
+        if (ipAddress.Equals("error")) return;
+        
+        var steamid = player.AuthorizedSteamID!.SteamId64.ToString();
 
         var admin = AdminManager.GetPlayerAdminData(new SteamID(player.SteamID));
         if (admin?.GetAllFlags().Contains("@css/reservation") ?? false) return;
 
         Task.Run(async () =>
         {
-            var isVPN = await IsIpVPN(ipAddress);
-            Server.NextFrame(() =>
+            var isVpn = await IsIpVpn(ipAddress);
+            await Server.NextFrameAsync(() =>
             {
                 AddTimer(5f, () =>
                 {
-                    if (!player.IsValid) return;
-                if (isVPN)
-                {
+                    if (!player.IsValid  || !isVpn) return;
+                    
                     player.Disconnect(NetworkDisconnectionReason.NETWORK_DISCONNECT_REJECTED_BY_GAME);
                     Logger.LogInformation($"Player {player.PlayerName} ({steamid}) ({ipAddress}) has been kicked. (VPN Usage)");
-                }
                 }, TimerFlags.STOP_ON_MAPCHANGE);
             });
 
         });
     }
-    static async Task <bool>IsIpVPN(string ipAddress)
-    {
-        using (var client = new HttpClient())
-        {
-            string requestURL = $"https://blackbox.ipinfo.app/lookup/{ipAddress}";
 
-            HttpResponseMessage response = await client.GetAsync(requestURL);
-            if (response.IsSuccessStatusCode)
-            {
-                string jsonResponse = await response.Content.ReadAsStringAsync();
-                if(jsonResponse == "Y"){
-                    return true;
-                }
-                else{
-                    return false;
-                }
-            }
-            return false;
-        }
+    private async Task <bool>IsIpVpn(string ipAddress)
+    {
+        using var client = new HttpClient();
+        var requestUrl = $"https://blackbox.ipinfo.app/lookup/{ipAddress}";
+
+        var response = await client.GetAsync(requestUrl);
+        if (!response.IsSuccessStatusCode) return false;
+        
+        var jsonResponse = await response.Content.ReadAsStringAsync();
+        return jsonResponse == "Y";
     }
 }
